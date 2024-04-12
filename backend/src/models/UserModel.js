36cf4cbd6860d092ace99af0bb58ec0735e29dbb
VasylynaSidei <vasilisasidey13@gmail.com>
2024-04-12 13:51:40 +0200

    add usermodel, productmodel, reviewmodel, adressmodel and connecttionDB

---
+++ backend/src/models/UserModel.js
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+import { Address } from "./AddressModel";
+
+const required = true;
+const unique = true;
+
+const userSchema = new Schema({
+  firstName: { type: String, required },
+  lastName: { type: String, required },
+  email: { type: String, required, unique },
+  password: { type: String, required },
+  address: { type: Address.schema, required },
+  phoneNumber: { type: String },
+  bonusPoints: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const User = model("User", userSchema);
