03642d352d27b8d18985ef536c133f757ed2ed3d
Balasim Jasim <balasimboliewi@gmail.com>
2024-04-30 13:36:54 +0200

    userContext/reducer&registration improved

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,15 @@
+/* eslint-disable no-unused-vars */
 import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./components/contexts/UserContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </BrowserRouter>
 );
