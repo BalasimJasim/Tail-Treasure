3e322d4c4036b9bc1b04dd996a93bc348eced2e1
kiriakosVangel <kiriakos_vaggeltzikis@hotmail.com>
2024-05-02 14:47:14 +0200

    adding reset and  forget password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import Register from "./components/pages/Register";
 import Terms from "./components/pages/Terms";
 import Dashboard from "./components/pages/Dashboard";
 import Login from "./components/pages/Login";
+import Reset from "./components/pages/Reset";
+import Forgot from "./components/pages/Forgot";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/terms" element={<Terms />} />
+        <Route path="/reset" element={<Reset />} />
+        <Route path="/forgot" element={<Forgot />} />
       </Routes>
     </>
   );
