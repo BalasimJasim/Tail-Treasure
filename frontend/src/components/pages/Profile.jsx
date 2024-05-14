bd3f199eca96fcc090d3f3c07ddbdd99056b1103
BalasimJasim <136313676+BalasimJasim@users.noreply.github.com>
2024-05-14 09:36:44 +0200

    Revert "styling profile page"

--- frontend/src/components/pages/Profile.jsx
+++ frontend/src/components/pages/Profile.jsx
@@ -1,30 +1,22 @@
-// export default Profile;
 /* eslint-disable react/no-unescaped-entities */
-import React, { useState } from "react";
+// eslint-disable-next-line no-unused-vars
+import React from "react";
 import "./profile.css";
-
 import av1 from "../../../images/av1.png";
 
 function Profile() {
-  const [isEditing, setIsEditing] = useState(false);
-
-  // Function to handle settings change
-  function handleSettingsChange() {
-    console.log("button is working");
-    setIsEditing(!isEditing);
-  }
-
   return (
     <div className="dash-container">
       <header className="dash-header">
         <h1>Profile</h1>
+
         <nav className="dash-sidebar">
           <ul>
             <li>
-              <a href="/">Home</a>
+              <a href="#">Home</a>
             </li>
             <li>
-              <a href="/CustomerSupportForm">Support</a>
+              <a href="CustomerSupportForm">Support</a>
             </li>
           </ul>
         </nav>
@@ -47,62 +39,13 @@ function Profile() {
         <div className="profile-information">
           <h2>Balasim's Profile Information</h2>
           <div className="profile-details">
-            <div>
-              <label htmlFor="name">Name:</label>
-              <span className={isEditing ? "hidden" : "info"}>Balasim</span>
-              <input
-                type="text"
-                id="name"
-                className={`input-field ${isEditing ? "" : "hidden"}`}
-              />
-            </div>
-            <div>
-              <label htmlFor="lastname">Lastname:</label>
-              <span className={isEditing ? "hidden" : "info"}>Balasimos</span>
-              <input
-                type="text"
-                id="lastname"
-                className={`input-field ${isEditing ? "" : "hidden"}`}
-              />
-            </div>
-            <div>
-              <label htmlFor="email">Email:</label>
-              <span className={isEditing ? "hidden" : "info"}>
-                dr.Balasimos@hotmail.com
-              </span>
-              <input
-                type="email"
-                id="email"
-                className={`input-field ${isEditing ? "" : "hidden"}`}
-              />
-            </div>
-            <div>
-              <label htmlFor="address">Address:</label>
-              <span className={isEditing ? "hidden" : "info"}>
-                BabylonStraße-3
-              </span>
-              <input
-                type="text"
-                id="address"
-                className={`input-field ${isEditing ? "" : "hidden"}`}
-              />
-            </div>
-            <div>
-              <label htmlFor="region">Region:</label>
-              <span className={isEditing ? "hidden" : "info"}>Wukanda</span>
-              <input
-                type="text"
-                id="region"
-                className={`input-field ${isEditing ? "" : "hidden"}`}
-              />
-            </div>
+            <p>Name: Balasim</p>
+            <p>Lastname: Balasimos</p>
+            <p>Email: dr.Balasimos@hotmail.com</p>
+            <p>Address: BabylonStraße-3</p>
+            <p>Region: Wukanda</p>
           </div>
-          <button
-            className="change-settings-btn"
-            onClick={handleSettingsChange}
-          >
-            {isEditing ? "Save Changes" : "Change Settings"}
-          </button>
+          <button className="change-settings-btn">Change Settings</button>
         </div>
       </main>
       <div className="navigation-buttons">
