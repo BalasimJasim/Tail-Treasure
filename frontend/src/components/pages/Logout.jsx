233b3e310782baf0e7531dd7536c911d3f2529c9
Balasim Jasim <balasimboliewi@gmail.com>
2024-05-20 16:47:43 +0200

    review/rating modified. error in guest user eleminated

--- frontend/src/components/pages/Logout.jsx
+++ frontend/src/components/pages/Logout.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Logout = () => {
+  const logoutHandler = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    console.log("logut:");
+  };
   return (
     <div>
       <p>You have been successfully logged out.</p>
-      <Link to="/login">Login</Link>
+      <Link onClick={logoutHandler} to="#">
+        logout
+      </Link>
     </div>
   );
 };
